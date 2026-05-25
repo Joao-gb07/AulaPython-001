227020367945f1dabac2d316a69ada0754f0d8cb
João Victor <va60234@gmail.com>
2026-05-25 08:33:48 -0300

    Update AplicarDescontoProduto.py

--- Aula Python/AplicarDescontoProduto.py
+++ Aula Python/AplicarDescontoProduto.py
@@ -1,7 +1,7 @@
 valor_produto = float(input("Digite o valor do produto R$ "))
 
 calculo_desconto1 = valor_produto * 0.10
-calculo_desconto2 = valor_produto * 0.5
+calculo_desconto2 = valor_produto * 0.05
 
 if (valor_produto >100):
     print(f"Você ganhou um desconto de R$ {calculo_desconto1} e irá pagar R$ {valor_produto - calculo_desconto1}")
